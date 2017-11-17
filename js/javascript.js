f30c09b5b0bdad9b19d4c24bee63d39d8e589d6c
Chris Dickson <chris.dickson1994@gmail.com>
2017-11-17 12:14:02 +0000

    initial commit of folders

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+function getFileList() {
+    document.getElementById("content").innerHTML = "List of files will show here";
+}
+
+function showImage() {
+    document.getElementById("content").innerHTML = "Images will be shown here."
+}
+
+function test() {
+    
+}
